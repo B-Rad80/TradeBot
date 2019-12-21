5f17edebf000262396a1aa0d9113393f558fe5eb
Brandon Roemer <brro7863@colorado.edu>
2019-12-21 01:07:48 -0700

    Update TradeBot.py
    grabbed api getting function

--- TradeBot/TradeBot.py
+++ TradeBot/TradeBot.py
@@ -1,4 +1,17 @@
+import json
+import requests
 import os
 
-print(os.getcwd())
-print("hello world")
+
+def Get_API_Key(path = "../../Alpha_API.key"):
+    f = open(path, "r")
+    API_KEY = str(f.read())
+
+    if "\n" in API_KEY:
+           API_KEY = API_KEY[:-1]
+
+    return API_KEY
+
+if __name__ == "__main__":
+    key = Get_API_Key()
+    print(key, "is the API key!")
